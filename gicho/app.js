158d520e2959d8bdbe43a8f0cb0286c422a5b3a5
JeongJaeyun99 <wodbs0723@ut.ac.kr>
2024-06-17 23:46:30 +0900

    input_output_test2

--- gicho/app.js
+++ gicho/app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const filePath = process.platform == 'linux' ? '/dev/stdin' : './input.txt';
+const filePath = process.platform == 'linux' ? '/dev/stdin' : './gicho/input.txt';
 let input = fs.readFileSync(filePath).toString().split('/n')
 
 input = input[0]
