35310de8bff10c2fc200f2ffb44a6d8b28a990ce
JeongJaeyun99 <wodbs0723@ut.ac.kr>
2024-06-17 23:42:31 +0900

    input_output_test

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform == 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('/n')
+
+input = input[0]
+input = input.split(' ').map((item => +item))
+console.log(solution(input[0],input[1]))
+
+function solution(a , b){
+    return a + b
+}
--- lecture01.js
+++ lecture01.js
@@ -1,3 +1,10 @@
-alert("ralro")
-alert("ralro")
-alert("ralro")
+var a = 1
+let b = 2
+const c = 1
+
+function add(x,y){
+    let temp = x + y
+    return temp
+}
+
+console.log(add(a,b))
